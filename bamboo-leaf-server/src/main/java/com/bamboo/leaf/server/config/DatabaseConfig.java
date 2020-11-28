339220ed8855e6cc657e82e174d8fdb9950d9e93
竹智 <sunney888@qq.com>
2020-11-29 00:24:35 +0800

    优化

--- bamboo-leaf-server/src/main/java/com/bamboo/leaf/server/config/DatabaseConfig.java
+++ bamboo-leaf-server/src/main/java/com/bamboo/leaf/server/config/DatabaseConfig.java
@@ -1,12 +1,16 @@
 package com.bamboo.leaf.server.config;
 
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
+import java.util.LinkedList;
 
 /**
  * @description: TODO
@@ -15,16 +19,41 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class DatabaseConfig {
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.druid")
-    public DataSource dataSource(){
-        return new DruidDataSource();
-    }
-
     @Bean
     public JdbcTemplate jdbcTemplate() {
         // 这里要将配置的 DruidDataSource 数据源注入 JdbcTemplate 中，不然默认注入 Spring Boot 自带的 HikariDatasource。
-        return new JdbcTemplate(dataSource());
+        return new JdbcTemplate(druidDataSource());
+    }
+
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public DruidDataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+
+        // 添加druid的监控过滤器，当前只演示监控的功能，因此只有一个过滤器，可以实现多个过滤器
+        LinkedList<Filter> filtersList = new LinkedList();
+        filtersList.add(filter());
+        druidDataSource.setProxyFilters(filtersList);
+
+        return druidDataSource;
+    }
+
+
+    @Bean
+    public Filter filter() {
+        StatFilter statFilter = new StatFilter();
+        // SQL执行时间超过2s种的被判定为慢日志
+        statFilter.setSlowSqlMillis(2000);
+        //显示慢日志
+        statFilter.setLogSlowSql(true);
+        //合并SQL，有时，一些相同的慢日志过多影响阅读，开启合并功能
+        statFilter.setMergeSql(true);
+        return statFilter;
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        // 注册自己的Sevlet
+        return new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
     }
 }
--- bamboo-leaf-server/src/main/java/com/bamboo/leaf/server/controller/LeafController.java
+++ bamboo-leaf-server/src/main/java/com/bamboo/leaf/server/controller/LeafController.java
@@ -4,9 +4,10 @@ import com.bamboo.leaf.server.dao.entity.TokenDO;
 import com.bamboo.leaf.server.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author lichunming
@@ -17,12 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class LeafController {
     private static final Logger logger = LoggerFactory.getLogger(LeafController.class);
 
-    @Autowired
+    @Resource
     TokenService tokenService;
 
     @RequestMapping("/insertToken")
     public int insertToken(TokenDO tokenDO) {
         return tokenService.insertToken(tokenDO);
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        logger.info("hello success");
+        return "success";
     }
 
 }
