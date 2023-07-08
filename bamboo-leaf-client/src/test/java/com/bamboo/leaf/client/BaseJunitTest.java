78aee25a5105ce50265b952188631f334d536a56
竹智 <sunney888@qq.com>
2023-07-08 23:36:00 +0800

    tomcat 切换到Undertow

--- bamboo-leaf-client/src/test/java/com/bamboo/leaf/client/BaseJunitTest.java
+++ bamboo-leaf-client/src/test/java/com/bamboo/leaf/client/BaseJunitTest.java
@@ -1,13 +1,10 @@
 package com.bamboo.leaf.client;
 
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class BaseJunitTest {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 }
